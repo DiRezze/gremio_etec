21174c61cce69c6fa4c0a5dd5b07cdd7ad391a8e
gabzz06666 <gabi.coracini.machado@gmail.com>
2025-04-25 20:40:41 -0300

    feat(calendar): add olympics events to calendar

--- src/content/olimpiadas.ts
+++ src/content/olimpiadas.ts
@@ -5,11 +5,124 @@ export const olimpiadas: Evento[] = [
   {
     nome: "OBMEP - Prova da 1ª fase",
     data: "2025-06-03T00:00:00-03:00",
-    cor: "#14AFCD",
+    cor: "#53c92e",
   },
   {
     nome: "OBMEP - Divulgação dos classificados para a 2ª fase",
     data: "2025-08-01T00:00:00-03:00",
-    cor: "#14AFCD",
+    cor: "#53c92e",
+  },
+  {
+    nome: "OBMEP - Divulgação dos locais de prova",
+    data: "2025-08-28T00:00:00-03:00",
+    cor: "#53c92e",
+  },
+  {
+    nome: "OBMEP - Prova da 2ª fase",
+    data: "2025-10-25T00:00:00-03:00",
+    cor: "#53c92e",
+  },
+  {
+    nome: "OBMEP - Divulgação dos premiados",
+    data: "2025-12-22T00:00:00-03:00",
+    cor: "#53c92e",
+  },
+  // ONHB
+  {
+    nome: "ONHB - Ínicio da Primeira fase",
+    data: "2025-05-05T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Primeira fase",
+    data: "2025-05-10T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Ínicio da Segunda fase",
+    data: "2025-05-12T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Segunda fase",
+    data: "2025-05-17T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Ínicio da Terceira fase",
+    data: "2025-05-19T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Terceira fase",
+    data: "2025-05-24T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Ínicio da Quarta fase",
+    data: "2025-05-26T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Quarta fase",
+    data: "2025-05-31T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Ínicio da Quinta fase",
+    data: "2025-06-02T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Quinta fase",
+    data: "2025-06-07T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Ínicio da Sexta fase",
+    data: "2025-06-09T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Término da Sexta fase",
+    data: "2025-06-14T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Divulgação das equipes selecionadas para a fase final",
+    data: "2025-06-21T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Grande Final Presencial - Prova",
+    data: "2025-08-30T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  {
+    nome: "ONHB - Cerimônia de Premiação",
+    data: "2025-08-31T00:00:00-03:00",
+    cor: "#ff00cc",
+  },
+  // OBA
+  {
+    nome: "OBA - Data de realização da prova",
+    data: "2025-05-16T00:00:00-03:00",
+    cor: "#ff6200",
+  },
+  {
+    nome: "OBA - Data de divulgação dos gabaritos",
+    data: "2025-05-18T00:00:00-03:00",
+    cor: "#ff6200",
+  },
+  // OLITEF
+  {
+    nome: "OLITEF - Realização da prova",
+    data: "2025-09-09T00:00:00-03:00",
+    cor: "#00ffe1",
+  },
+  {
+    nome: "OLITEF - Resultados da Olímpiada",
+    data: "2025-10-30T00:00:00-03:00",
+    cor: "#00ffe1",
   },
 ];
